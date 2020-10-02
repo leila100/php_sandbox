1cca52355da01bbda301954d63a99d8e324bac20
Leila <nb.leila10@gmail.com>
2020-10-02 16:46:19 -0500

    Show the syntax of using AJAX call with PHP and javascript

---
+++ website6/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Search User</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script>
+        function showSuggestion(str) {
+            const element = document.getElementById('output');
+            if (str.length == 0) {
+                element.innerHTML = ""
+            } else {
+                //AJAX request
+                var xmlhttp = new XMLHttpRequest();
+                xmlhttp.onreadystatechange = function() {
+                    if (this.readyState == 4 && this.status == 200) {
+                        element.innerHTML = this.responseText;
+                    }
+                }
+                xmlhttp.open("GET", "suggest.php?q=" + str, true);
+                xmlhttp.send();
+            }
+        }
+    </script>
+</head>
+
+<body>
+    <div class="container">
+        <h1>Search Users</h1>
+        <form>
+            Search User: <input type="text" class="form-control" onkeyup="showSuggestion(this.value)">
+        </form>
+        <p>Suggestions: <span id="output" style="font-weight: bold"></span></p>
+    </div>
+</body>
+
+</html>
