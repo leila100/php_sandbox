df23884e0b6a7c0e7ba7617c5fd1ad9ae82b9d5f
Leila <nb.leila10@gmail.com>
2020-10-05 12:14:01 -0500

    Setup connection to database in db.php

---
+++ website8/config/db.php
@@ -0,0 +1,11 @@
+<?php
+//   Create connect to database
+$conn = mysqli_connect('localhost', 'root', 'leilaberrouayel', 'phpblog');
+
+//Check Connection
+if (mysqli_connect_errno()) {
+    // Connection Failed
+    echo 'Failed to connect ot database' . mysqli_connect_errno();
+} else {
+    echo 'Connected to database';
+}
