11b79534cb796e45cc21bdbe9958f20107acd9e0
Leila <nb.leila10@gmail.com>
2020-10-05 13:39:55 -0500

    Add Navbar

---
+++ website8/inc/navbar.php
@@ -0,0 +1,9 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">PHP BLOG</a>
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" href="<?php echo ROOT_URL; ?>">Home</a>
+        </li>
+    </ul>
+</nav>
+<br>
