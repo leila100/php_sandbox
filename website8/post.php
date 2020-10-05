3db9844f10b088e539d35b15b39166ee9223db1a
Leila <nb.leila10@gmail.com>
2020-10-05 13:28:52 -0500

    Add footer and header to minimize repeated code

--- website8/post.php
+++ website8/post.php
@@ -22,25 +22,14 @@ mysqli_close($conn);
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
+<?php include('inc/header.php'); ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Blog</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-</head>
-
-<body>
-    <div class="container">
-        <br>
-        <a href="<?php echo ROOT_URL; ?>" class="btn btn-primary">Back</a>
-        <br>
-        <h1><?php echo $post['title']; ?></h1><br>
-        <p><?php echo $post['body']; ?></p>
-        <small>Created on <?php echo $post['created_at']; ?> by <?php echo $post['author']; ?></small>
-    </div>
-</body>
-
-</html>
+<div class="container">
+    <br>
+    <a href="<?php echo ROOT_URL; ?>" class="btn btn-primary">Back</a>
+    <br>
+    <h1><?php echo $post['title']; ?></h1><br>
+    <p><?php echo $post['body']; ?></p>
+    <small>Created on <?php echo $post['created_at']; ?> by <?php echo $post['author']; ?></small>
+</div>
+<?php include('inc/footer.php'); ?>
